650112e92e43b7b377e80909f2f7e86fe113e5d2
AgueroZZ <49107284+AgueroZZ@users.noreply.github.com>
2021-08-28 17:33:49 -0400

    update research comps

---
+++ Consistency_ARIMA.R
@@ -0,0 +1,36 @@
+##################################################################################################
+########### Exploring the consistency of ARIMA method for different resolutions ###################
+##################################################################################################
+
+#### Load all the needed functions:
+compute_H_rue <- function(d,n){
+  H <- matrix(data = 0, nrow = n, ncol = n)
+  for (i in 2:(nrow(H)-1)) {
+    H[i,i] <- -(1/d[i-1]+1/d[i])
+    H[i,i-1] <- 1/d[i-1]
+    H[i,i+1] <- 1/d[i]
+  }
+  H
+}
+compute_B <- function(d,n){
+  B <-matrix(0, nrow = n, ncol = n)
+  B[1,1] <- d[1]/3
+  B[1,2] <- d[1]/6
+  B[n,n-1] <- d[n-1]/6
+  B[n,n] <- d[n-1]/3
+  for (i in 2:(nrow(B)-1)) {
+    B[i,i-1] <- d[i-1]/6
+    B[i,i] <- (d[i-1]+d[i])/3
+    B[i,i+1] <- d[i]/6
+  }
+  B
+}
+compute_A <- function(d,n){
+  A <-matrix(0, nrow = n, ncol = n)
+  A[1,1] <- d[1]/2
+  A[n,n] <- d[n-1]/2
+  for (i in 2:(nrow(A)-1)) {
+    A[i,i] <- (d[i-1]+d[i])/2
+  }
+  A
+}
